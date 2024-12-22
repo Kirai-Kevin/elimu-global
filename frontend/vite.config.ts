85e4d97f945ad6be528e4c1f22b60332620b9e1f
Kirai-Kevin <kiraikevin627@gmail.com>
2024-12-22 03:16:05 +0300

    345

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -32,4 +33,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
